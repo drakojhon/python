50a84f057161a558c0982b873bab4e028bae1c3f
JONATHAN JESUS CHAN CETZAL <antrax_drako@hotmail.com>
2017-03-28 12:46:38 -0600

    se actualizo el archivo

---
+++ tareau2pythonv2.py
@@ -0,0 +1,72 @@
+#1. Realiza un programa que permita obtener la media de 3 números.  	
+def media_3(X, Y, Z):
+        media = (float(X) + float(Y) +float(Z))/3
+        print "media = ", media
+#2. Realiza una función que permita obtener el volumen de una esfera.
+def vol_Esf(radio):
+        vol = (1.33 * 3.141516 ) * (float(radio)**3)
+        print "El volumen es:", vol
+
+#3: 10 numeros impares inciando con en 13
+
+def impares(x):
+
+ while x <= 32: 
+    if x%2 != 0:
+        y += ' %i' % x
+    n += 13
+print y
+ 
+        
+#4. Realiza una función que permita obtener el máximo de 3 números.
+def mayor_N( n1, n2, n3):
+
+print( n1, n2, n3)
+
+
+if n2< n1 >n3:
+        print(n1)
+if n1< n2 >n3:
+        print("el mayor es el primer numero :",n2)
+if n1< n3 >n2:
+        print("el mayor es el primer numero :",n3)
+
+# 5 rotar una lista
+# 6 Realiza un programa que permita generar un intervalo de la suma de los cubos de los primeros n números. Ejemplo suma = 1 + 8 + 27 + n
+def intervaloSuma(x):      
+        vacio=[(conta**3) for conta in range(1,x+1)]        
+        print vacio
+        return sum(vacio)
+#7. Realiza un programa que permita generar un intervalo de los cuadrados de n números mayores a 100.
+sumMayor n=[x^2 | x <- [1..n], x^2>100]
+def intervaloMax(x):
+           vacio=[(conta**2) for conta in range(1,x+1)]        
+        num=1
+        while num<len(vacio):
+                if vacio[num]>100:
+                        print vacio[num]
+                num+=1
+#8. Realiza un programa que permita generar un intervalo de n numeros entre 20 y 60
+def interva20(x):
+        cont=1
+        while cont<=x:
+                if cont>=20 and cont<=60:
+                        print cont
+                cont+=1
+
+#9. Realiza un programa que solicite dos argumentos de tipo Double para calcular la hipotenusa de un triángulo rectángulo y retorne un valor de tipo Double.
+
+def hipoT (x, y):
+      hipotenusa = math.sqrt((x*x) +(y*y)
+print "La hipotenusa es:", hipotenusa
+
+#10. Crear un programa que por medio de recursión calcule la suma de los cuadrados de n números.
+
+def recursive (dato, variable=0):
+        if(dato>=0):
+                variable +=dato**2
+                return recu(dato-1,variable)
+        else:
+                print variable
+
+
